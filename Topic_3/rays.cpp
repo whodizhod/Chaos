90c0cb793f07e960079ac29ebeb04e4422a4be78
whodizhod <whodizhod@gmail.com>
2023-03-28 01:57:06 +0300

    Added new functions; Cleared useless code.

--- Topic_3/rays.cpp
+++ Topic_3/rays.cpp
@@ -1,32 +1,59 @@
 #include <array>
-#include <string>
-#include <sstream>
-#include <random>
 #include <fstream>
 #include <cmath>
 #include "vec3.h"
 
 // Output image resolution
-static const int imageWidth = 30;
-static const int imageHeight = 30;
+static const int imageWidth = 1920;
+static const int imageHeight = 1080;
 
 static const int maxColorComponent = 255;
+static const float pixelCenter = 0.5;
 
-using bitmap = std::array<std::array<Vec3f, imageHeight>, imageWidth>;
+using Vec3f = Vec3 <float>;
+using Bitmap = std::array <std::array <Vec3f, imageHeight>, imageWidth>;
 
-int main() {
-	static bitmap bitMap;
-	Vec3f origin;
-
+// Converts coordinates to NDC space
+void toNDC(Bitmap& bitMap) {
 	for (int y = 0; y < imageHeight; ++y) {
 		for (int x = 0; x < imageWidth; ++x) {
-			bitMap[x][y].x = int(abs((((2.0 * ((x + 0.5) / imageWidth)) - 1.0) * (imageWidth / imageHeight)) * 255));
-			bitMap[x][y].y = int(abs((1.0 - (2.0 * ((y + 0.5) / imageHeight))) * 255));
-			bitMap[x][y].z = int(abs( - 1.0f * 255));
+			bitMap[x][y].x = int((x + pixelCenter) / imageWidth * maxColorComponent);
+			bitMap[x][y].y = int((y + pixelCenter) / imageHeight * maxColorComponent);
+			bitMap[x][y].z = int(abs(-1.0f * maxColorComponent));
 		}
 	}
+}
 
-	std::ofstream ppmFileStream("rays.ppm", std::ios::out | std::ios::binary);
+// Converts coordinates to Screen space
+// Without aspect ratio
+void toScreenSpace(Bitmap& bitMap) {
+	for (int y = 0; y < imageHeight; ++y) {
+		for (int x = 0; x < imageWidth; ++x) {
+			bitMap[x][y].x = int(abs((2.0f * ((x + pixelCenter) / imageWidth) - 1.0f) * maxColorComponent));
+			bitMap[x][y].y = int(abs((1.0f - (2.0f * ((y + pixelCenter) / imageHeight))) * maxColorComponent ));
+			bitMap[x][y].z = int(abs(-1.0f * maxColorComponent));
+		}
+	}
+}
+
+// Converts coordinates to Screen space
+// With aspect ratio
+void toScreenSpaceAspect(Bitmap& bitMap) {
+	for (int y = 0; y < imageHeight; ++y) {
+		for (int x = 0; x < imageWidth; ++x) {
+			bitMap[x][y].x = int(abs(((2.0f * ((x + pixelCenter) / imageWidth)) - 1.0f) * (float(imageWidth)/float(imageHeight)) * maxColorComponent));
+			bitMap[x][y].y = int(abs((1.0f - (2.0f * ((y + pixelCenter) / imageHeight))) * maxColorComponent));
+			bitMap[x][y].z = int(abs(-1.0f * maxColorComponent));
+		}
+	}
+}
+
+int main() {
+	static Bitmap bitMap;
+
+	toScreenSpaceAspect(bitMap);
+
+	std::ofstream ppmFileStream("Screen_space_z255_aspect.ppm", std::ios::out | std::ios::binary);
 	ppmFileStream << "P3\n";
 	ppmFileStream << imageWidth << " " << imageHeight << "\n";
 	ppmFileStream << maxColorComponent << "\n";
