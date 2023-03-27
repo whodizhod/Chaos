95577bbee0f1d1a2776ee6597d86dd7fbe5304fa
whodizhod <whodizhod@gmail.com>
2023-03-28 01:55:55 +0300

    Fixed vec3.h; Added directives;

--- Topic_3/vec3.h
+++ Topic_3/vec3.h
@@ -1,6 +1,8 @@
-template<typename T>
-class Vec3
-{
+#ifndef VEC3_H
+#define VEC3_H
+
+template <typename T>
+class Vec3 {
 public:
 	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
 	explicit Vec3(const T& xx) : x(xx), y(xx), z(xx) {}
@@ -10,5 +12,4 @@ public:
 	T y;
 	T z;
 };
-
-using Vec3f = Vec3<float>;
+#endif
